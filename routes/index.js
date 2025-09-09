442d638530a72287dedd54d7c69ca5518e674ac8
LokiObahoshe <sondereggerbecca@gmail.com>
2025-09-09 15:59:09 -0500

    swagger integration

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,13 @@
 const routes = require('express').Router();
 const temple = require('./temple');
 
+routes.use('/', require('./swagger'));
 routes.use('/temples', temple);
-routes.use(
-  '/',
-  (docData = (req, res) => {
-    let docData = {
-      documentationURL: 'https://nathanbirch.github.io/nathan-byui-api-docs',
-    };
-    res.send(docData);
-  })
-);
+routes.use('/', (req, res) => {
+  const docData = {
+    documentationURL: 'https://johndoe.github.io/john-api-docs',
+  };
+  res.send(docData);
+});
 
 module.exports = routes;
